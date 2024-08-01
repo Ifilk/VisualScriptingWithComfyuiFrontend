f16c1d401f014eaf5b0329664cbde9d89bbd917b
suanxc <suanxc@yeah.net>
2024-08-01 08:52:51 +0800

    update

--- custom_nodes/ComfyUI-Math-Numpy/web/js/uploadMat.js
+++ custom_nodes/ComfyUI-Math-Numpy/web/js/uploadMat.js
@@ -3,7 +3,6 @@ import {app} from "/scripts/app.js";
 app.registerExtension({
     name: "base.LoadMat",
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        console.log(nodeData)
         if (nodeData?.input?.required?.file?.[1]?.file_upload === true) {
             nodeData.input.required.upload = ["FILEUPLOAD"];
         }
